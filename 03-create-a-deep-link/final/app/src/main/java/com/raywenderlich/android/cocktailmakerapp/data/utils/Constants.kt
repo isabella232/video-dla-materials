9e7533baa62ab62a191091aa303b34a5cce0d93c
Harun Wangereka <harunwangereka@gmail.com>
2021-12-09 17:04:14 +0300

    Removing idea files

--- 03-create-a-deep-link/final/app/src/main/java/com/raywenderlich/android/cocktailmakerapp/data/utils/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.raywenderlich.android.cocktailmakerapp.data.utils
-
-object Constants {
-  const val BASE_URL = "https://www.thecocktaildb.com/api/json/v1/1/"
-}
--- 04-get-data-from-deep-links/starter/app/src/main/java/com/raywenderlich/android/cocktailmakerapp/vm/UIState.kt
+++
@@ -1,7 +0,0 @@
-package com.raywenderlich.android.cocktailmakerapp.vm
-
-sealed class UIState {
-  object Loading : UIState()
-  data class ShowData<T>(val data: T) : UIState()
-  data class Error(val message: String) : UIState()
-}
